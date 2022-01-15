cae4a9e8332f9d73a379393af33fd18b896527c8
RakibRahman <md.rr.talukder@gmail.com>
2022-01-15 19:13:37 +0600

    hashtag and mentions

--- src/Create.js
+++ src/Create.js
@@ -96,6 +96,21 @@ export const Create = () => {
                 wrapperClassName="wrapperClassName"
                 editorClassName="editorClassName"
                 onEditorStateChange={(editorState) => setEditorState(editorState)}
+                mention={{
+                    separator: ' ',
+                    trigger: '@',
+                    suggestions: [
+                        { text: 'APPLE', value: 'apple', url: 'facebook.com' },
+                        { text: 'BANANA', value: 'banana', url: 'banana' },
+                        { text: 'CHERRY', value: 'cherry', url: 'cherry' },
+                        { text: 'DURIAN', value: 'durian', url: 'durian' },
+                        { text: 'EGGFRUIT', value: 'eggfruit', url: 'eggfruit' },
+                        { text: 'FIG', value: 'fig', url: 'fig' },
+                        { text: 'GRAPEFRUIT', value: 'grapefruit', url: 'grapefruit' },
+                        { text: 'HONEYDEW', value: 'honeydew', url: 'honeydew' },
+                    ],
+                }}
+                hashtag={{}}
             />
 
             <Box
